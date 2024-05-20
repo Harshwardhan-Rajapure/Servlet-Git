0b0db3f71ba6b3c0655ebfe436c75fed6370c3dd
Harshwardhan Rajapure <118995657+Harshwardhan-Rajapure@users.noreply.github.com>
2024-05-20 19:06:37 +0530

    Add files via upload

---
+++ Servlet-1/DemoJSP/src/main/java/com/tka/servlet/LoginServlet.java
@@ -0,0 +1,31 @@
+package com.tka.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public LoginServlet() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//doGet(request, response);
+		
+		System.out.println("U r in do post Method...");
+		
+	}
+
+}
